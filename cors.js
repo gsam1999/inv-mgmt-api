aad7ac5c8d51aafe68b910ecd757b447ebfb60fc
sumanth <gsumanth@protonmail.com>
2021-09-29 12:08:27 +0530

    Integration with the UI

--- cors.js
+++ cors.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 
-const whiteList = ['https://localhost:3443', 'http://localhost:3000', 'https://localhost:3443']
+const whiteList = ['https://localhost:3443', 'http://localhost:3000', 'https://localhost:3443', 'https://gsam1999.github.io']
 
 var corsOptionDelegate = (req, callback) => {
     var corsOptions;
--- routes/items.js
+++ routes/items.js
@@ -5,6 +5,7 @@ const cors = require('../cors')
 const itemRouter = express.Router();
 
 // let item = class {
+//     id
 //     name;
 //     type;
 //     measurement;
@@ -13,6 +14,7 @@ const itemRouter = express.Router();
 // };
 
 var items = [];
+var transactions = [];
 
 itemRouter.use(bodyParser.json());
 
@@ -20,13 +22,73 @@ itemRouter.route('/')
     .get(cors.cors, (req, res, next) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json(items);
+        res.send(items);
     })
     .post(cors.cors, (req, res, next) => {
+        req.body._id = new Date().getTime();
         items.push(req.body)
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json(req.body);
+        res.send(req.body);
+    })
+
+itemRouter.route('/:itemid')
+    .get(cors.cors, (req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.send(items.find(ele => ele._id == req.params.itemid));
+    })
+    .post(cors.cors, (req, res, next) => {
+        let item = items.find(ele => ele._id == req.params.itemid);
+        if (item) {
+            items[items.indexOf(item)] = req.body
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.send(req.body);
+        }
+        else {
+            err = new Error("item cannot be found");
+            err.statusCode = 404;
+            return next(err);
+        }
+    })
+
+itemRouter.route('/:itemid/UpdateQuantity')
+    .post(cors.cors, (req, res, next) => {
+        let item = items.find(ele => ele._id == req.body._id);
+        if (item) {
+            if (req.body.action == 'add')
+                item.quantity += Number(req.body.quantity);
+            else
+                item.quantity -= Number(req.body.quantity);
+            transactions.push({ itemid: req.body._id, data: req.body })
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.send(item);
+        }
+        else {
+            err = new Error("item cannot be found");
+            err.statusCode = 404;
+            return next(err);
+        }
+    })
+
+const transactionRouter = express.Router({ mergeParams: true });
+
+itemRouter.use('/:itemid/transactions', transactionRouter);
+
+transactionRouter.route('/')
+    .get(cors.cors, (req, res, next) => {
+        let item = items.find(ele => ele._id == req.params.itemid);
+        if (item) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(transactions.filter(ele => ele.itemid = req.params.itemid));
+        }
+        else {
+            err = new Error("item cannot be found");
+            err.statusCode = 404;
+            return next(err);
+        }
     })
 
 module.exports = itemRouter;
