85b9961ee5b83365b4f1504db936965ae13cefa2
sumanth <gsumanth@protonmail.com>
2021-10-30 19:57:08 +0530

    removed config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     TOKEN_KEY: '1234', // used to sign jwt tokens
-    mongoUrl: 'mongodb+srv://admin:admin@cluster0.vtf4u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoUrl: 'mongodb://' //mongo db uri
 }
